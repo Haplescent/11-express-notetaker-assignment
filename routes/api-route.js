f8b8e414c74b95f00e387a97a0cc353c84fec198
John Merritt <johnmerritt95@gmail.com>
2020-06-24 10:08:07 -0700

    then chainning on api-route

--- routes/api-route.js
+++ routes/api-route.js
@@ -7,29 +7,52 @@ const path = require("path");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-router.get("/api/notes", async (req, res) => {
-  const file = JSON.parse(await readFileAsync("./db/db.json", "utf8"));
-  res.send(file);
+router.get("/api/notes", (req, res) => {
+  // const file = JSON.parse(await readFileAsync("./db/db.json", "utf8"));
+  readFileAsync("./db/db.json", "utf8")
+    .then((file) => {
+      return JSON.parse(file);
+    })
+    .then((file) => {
+      res.send(file);
+    });
 });
 
 router.post("/api/notes", async (req, res) => {
-  const data = JSON.parse(await readFileAsync("./db/db.json", "utf8"));
-  const newNoteObject = req.body;
-  newNoteObject.id = data.length + 1;
-  data.push(newNoteObject);
-  await writeFileAsync("./db/db.json", JSON.stringify(data, null, 2));
-  res.send(data);
+  await readFileAsync("./db/db.json", "utf8")
+    .then((file) => {
+      return JSON.parse(file);
+    })
+    .then((data) => {
+      const newNoteObject = req.body;
+      newNoteObject.id = data.length + 1;
+      data.push(newNoteObject);
+      writeFileAsync("./db/db.json", JSON.stringify(data, null, 2));
+      return data;
+    })
+    .then((data) => {
+      res.send(data);
+    });
 });
+
 router.delete("/api/notes/:id", async (req, res) => {
-  const data = JSON.parse(await readFileAsync("./db/db.json", "utf8"));
-  const incomingID = req.params.id;
-  data.forEach((value, index) => {
-    if (value.id == incomingID) {
-      data.splice(index, 1);
-    }
-  });
-  await writeFileAsync("./db/db.json", JSON.stringify(data, null, 2));
-  res.send({ msg: "success" });
+  await readFileAsync("./db/db.json", "utf8")
+    .then((file) => {
+      return JSON.parse(file);
+    })
+    .then((data) => {
+      const incomingID = req.params.id;
+      data.forEach((value, index) => {
+        if (value.id == incomingID) {
+          data.splice(index, 1);
+        }
+      });
+      writeFileAsync("./db/db.json", JSON.stringify(data, null, 2));
+      return data;
+    })
+    .then(() => {
+      res.send({ msg: "success" });
+    });
 });
 
 module.exports = router;
